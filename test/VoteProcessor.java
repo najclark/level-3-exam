dbd6c751e21fba77e9a470c902881119447bf583
Nicholas AJ Clark <nicholas@californiaclarks.com>
2015-09-15 17:17:21 -0700

    I finished

--- test/VoteProcessor.java
+++ test/VoteProcessor.java
@@ -1,30 +1,30 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 
-
 public class VoteProcessor {
 
 	public String calculateElectionWinner(ArrayList<String> votes) {
 		HashMap<String, Integer> people = new HashMap<String, Integer>();
-		people.put("pope francis", 0);
-		people.put("edward snowden", 0);
 
-		for(String s : votes){
+		for (String s : votes) {
 			s = s.toLowerCase();
-			people.put(s, people.get(s) + 1);
+			if (people.containsKey(s)) {
+				people.put(s, people.get(s) + 1);
+			} else {
+				people.put(s, 1);
+			}
 		}
-		
+
 		int largest = 0;
 		String key = "";
-		for(String p : people.keySet()){
-			if(people.get(p) > largest){
+		for (String p : people.keySet()) {
+			if (people.get(p) > largest) {
 				key = p;
 				largest = people.get(p);
-			}
-			else if (people.get(p) == largest)
+			} else if (people.get(p) == largest)
 				key = "TIE";
 		}
-		
+
 		return key;
 	}
 
